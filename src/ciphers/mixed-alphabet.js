6b319f314c28081b3d526d751e73a652cdea708c
Mikael Löfstrand <micke@lofstrand.net>
2024-03-17 19:05:47 +0100

    npm upgrade and explicit include of eslint

---
+++ src/ciphers/mixed-alphabet.js
@@ -0,0 +1,18 @@
+const caesar = () => {
+  return {
+    id: "caesar",
+    name: "Caesar",
+    steps: true,
+    legend(alphabet, shift_characters = 1) {
+      let legend = {};
+
+      for (let step = 0; step < alphabet.length; step++) {
+        legend[alphabet[step]] = alphabet[(step + shift_characters) % alphabet.length];
+      }
+
+      return legend;
+    },
+  };
+};
+
+export default caesar();

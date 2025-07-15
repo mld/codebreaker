c081b645f30ae3db83065d0e2deac1282a47fcb6
Mikael Löfstrand <micke@lofstrand.net>
2025-07-15 22:29:29 +0200

    complete upgrade to tailwind4, including some symbol changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
   content: ["./index.html", "./main.js", "./src/**/*.js"],
-  theme: {
-    extend: {
-      fontFamily: {
-        emoji: ['"Noto Emoji Variable"'],
-      },
-    },
-  },
   plugins: [],
 };
